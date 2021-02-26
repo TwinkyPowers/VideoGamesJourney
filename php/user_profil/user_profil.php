762e7035808d5b1bb334b52b81a419536d014d95
laurent.s <saloppe.laurent@gmail.com>
2021-02-26 12:57:09 +0100

    maj40
    responsive for logo

--- php/user_profil/user_profil.php
+++ php/user_profil/user_profil.php
@@ -39,6 +39,12 @@ else
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/user_profil_style/stylesheet_user_profil.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../ressources/png/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../ressources/png/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../ressources/png/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../../ressources/png/favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <title>Profil | VideoGamesJourney</title>
 </head>
 <body>
